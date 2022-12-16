746a255e63445ce499d2882fcdc2a6a08028c80a
Guilherme Vanderley <guivdouradog@gmail.com>
2022-12-16 17:17:34 -0300

    init pagination

--- js/controllers/homeController.js
+++ js/controllers/homeController.js
@@ -1,11 +1,35 @@
 angular.module("mySchool").controller("homeController", function ($scope, students, config, localStorage, $location) {
   console.log(students.data);
-  $scope.alunos = students.data;
   $scope.baseUrl = config.baseUrl;
 
   $scope.signOut = () => {
     localStorage.removeToken();
     $location.path('login');
+  };
+
+  const formatPages = data => {
+    const pages = [];
+    let page = [];
+    for(aluno of data) {
+      page.push(aluno);
+      if(page.length === 6) {
+        pages.push(page);
+        page = [];
+      }
+    };
+    if(page) pages.push(page);
+    return pages;
+  };
+
+  $scope.pages = formatPages(students.data);
+  $scope.index = 0;
+
+  $scope.nextPage = () => {
+    $scope.index += 1;
+  };
+
+  $scope.backPage = () => {
+    $scope.index -= 1;
   };
 
 });
