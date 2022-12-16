cd20c05e06f6c716074d997852b8b90a61261aca
Guilherme Vanderley <guivdouradog@gmail.com>
2022-12-16 16:51:31 -0300

    fixed images and create signOut

--- js/controllers/homeController.js
+++ js/controllers/homeController.js
@@ -1,4 +1,11 @@
-angular.module("mySchool").controller("homeController", function ($scope, students) {
+angular.module("mySchool").controller("homeController", function ($scope, students, config, localStorage, $location) {
   console.log(students.data);
   $scope.alunos = students.data;
+  $scope.baseUrl = config.baseUrl;
+
+  $scope.signOut = () => {
+    localStorage.removeToken();
+    $location.path('login');
+  };
+
 });
