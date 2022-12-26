5c53fe455e6f5bef5635ed790b7491e1370f4dfb
Guilherme Vanderley <guivdouradog@gmail.com>
2022-12-26 02:30:02 -0300

    create login alert

--- js/controllers/loginController.js
+++ js/controllers/loginController.js
@@ -1,9 +1,15 @@
 angular.module("mySchool").controller("loginController", function($scope, $location,loginAPI, localStorage) {
+  $scope.alertLogin = false;
+  
   $scope.submitLogin = credentials => { 
     loginAPI.login(credentials)
       .then(resp => {
         localStorage.setToken(resp.data.token);
+        $scope.alertLogin = false;
         $location.path('/home');
-      }).catch(error => console.log(error));
+      }).catch(error => {
+        console.log(error);
+        $scope.alertLogin = true;
+      });
   };
 });
